77eebd224e519eaa972ec3ed5cd96c2236cf6ece
Magnus Billups <billups.clayton@gmail.com>
2016-10-26 21:58:50 -0500

    Add Order model and api controller. Link order model with Entity

---
+++ PizzaOrderingWebApp/PizzaOrderingWebApp/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PizzaOrderingWebApp.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int PizzasOrdered { get; set; }
+    }
+}
---
+++ PizzaOrderingWebApp/PizzaOrderingWebApp/Models/OrderDb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PizzaOrderingWebApp.Models
+{
+    public class OrderDb : DbContext
+    {
+        public DbSet<Order> orders { get; set; }
+    }
+}
