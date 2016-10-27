c05bdcde63edb6cf3a475544e22df2ade26375e4
Magnus Billups <billups.clayton@gmail.com>
2016-10-26 22:12:00 -0500

    Create and Integrate Angularjs module

--- PizzaOrderingWebApp/PizzaOrderingWebApp/Scripts/app/pizzaApp.js
+++ PizzaOrderingWebApp/PizzaOrderingWebApp/Scripts/app/pizzaApp.js
@@ -1,1 +1,23 @@
-﻿
+﻿var app = angular.module('PizzaApp', []);
+
+app.controller('OrderController', function ($scope) {
+    $scope.allOrders = [];
+    $scope.isOrderInProgress = true;
+    $scope.completeOrder = function () {
+        //create order from form data
+        //call orderService.addOrder
+        //add order to allOrders
+        //set isOrderInProgress to false
+    }
+});
+
+app.factory('orderService', function ($resource) {
+    return {
+        getAllOrders: function () {
+            return $resource('api/orders').get();
+        },
+        addOrder: function (order) {
+            $resource('api/order').save(order);
+        }
+    }
+})
